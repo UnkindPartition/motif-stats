55960a35a2a7521a2ac938ba894e73b236b56dc1
Roman Cheplyaka <roma@ro-che.info>
2018-07-31 01:39:20 +0100

    Further simplify

--- src/NFA.hs
+++ src/NFA.hs
@@ -10,41 +10,26 @@ import IUPAC (primitiveCodes)
 type NfaState = Int
 
 data NFA = NFA
-  { nfaFinal :: [NfaState]
+  { nfaFinal :: NfaState
   , nfaTransitions :: [(Char, NfaState, NfaState)]
   }
   deriving Show
 
-type Numbered = [[(NfaState, Char)]]
-
-numberSymbols :: [[Char]] -> Numbered
-numberSymbols = zipWith (map . (,)) [1..]
-
-follow :: Numbered -> [(Char, NfaState, NfaState)]
-follow motif = concat $ zipWith follow1 motif (tail motif)
+follow :: [[Char]] -> [(Char, NfaState, NfaState)]
+follow = concat . zipWith follow1 [0..]
   where
     follow1
-      :: [(NfaState, Char)]
-      -> [(NfaState, Char)]
+      :: NfaState
+      -> [Char]
       -> [(Char, NfaState, NfaState)]
-    follow1 this next = do
-      (this_state, _) <- this
-      (next_state, next_char) <- next
-      return (next_char, this_state, next_state)
-
-final :: Numbered -> [NfaState]
-final motif
-  | null motif = [0]
-  | otherwise = fst <$> last motif
+    follow1 n next =
+      [ (c, n, n+1) | c <- next ]
 
 -- | Construct an NFA that recognizes the motif preceeded by any number of
 -- characters
 motifToNFA :: [[Char]] -> NFA
 motifToNFA motif =
-  let
-    numbered = numberSymbols motif
-  in
-    NFA
-      { nfaFinal = final numbered
-      , nfaTransitions = follow numbered ++ [(c, 0, 0) | c <- primitiveCodes]
-      }
+  NFA
+    { nfaFinal = length motif
+    , nfaTransitions = follow motif ++ [(c, 0, 0) | c <- primitiveCodes]
+    }
