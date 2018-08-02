4551344172d0d91ec01db63fc4119fe1dd800b5c
Roman Cheplyaka <roma@ro-che.info>
2018-08-02 23:18:41 +0100

    Implement regexes

--- src/NFA.hs
+++ src/NFA.hs
@@ -1,14 +1,25 @@
 module NFA
-  ( NfaState(..)
+  ( Regex(..)
+  , NfaState(..)
   , NfaTransitions(..)
   , NFA(..)
+  , compile
   , motifToNFA
   )
   where
 
 import qualified Data.Set as Set
 import qualified Data.Map.Strict as Map
+import Control.Monad.State.Strict
 import IUPAC (primitiveCodes)
+
+data Regex c
+  = Sym c
+  | Seq (Regex c) (Regex c)
+  | Alt (Regex c) (Regex c)
+  | Plus (Regex c)
+  | Star (Regex c)
+  | Empty
 
 newtype NfaState = NfaState Int
   deriving (Eq, Ord, Show)
@@ -33,6 +44,81 @@ data NFA c = NFA
   , nfaTransitions :: NfaTransitions c
   }
   deriving Show
+
+compile :: Ord c => Regex c -> NFA (Maybe c)
+compile = flip evalState 0 . compileM
+
+-- Here I chose a fully recursive implementation (each recursive call
+-- produces a full NFA; auxiliary states glue them together), as opposed to
+-- a continuation-based semi-recursive implementation (a recursive call
+-- accepts exit states, returns start states and transitions) used in
+-- regex-applicative and in
+-- https://ro-che.info/articles/2015-09-02-monadfix. No particular reason.
+compileM
+  :: Ord c
+  => Regex c
+  -> State Int (NFA (Maybe c))
+compileM = \case
+  Sym c -> do
+    s <- fresh
+    f <- fresh
+    return $ NFA
+      { nfaStart = Set.singleton s
+      , nfaFinal = Set.singleton f
+      , nfaTransitions = transitionsFromList [(Just c, s, f)]
+      }
+  Empty -> do
+    s <- fresh
+    return $ NFA
+      { nfaStart = Set.singleton s
+      , nfaFinal = Set.singleton s
+      , nfaTransitions = transitionsFromList []
+      }
+  Seq a b -> do
+    nfa_a <- compileM a
+    nfa_b <- compileM b
+    return $ NFA
+      { nfaStart = nfaStart nfa_a
+      , nfaFinal = nfaFinal nfa_b
+      , nfaTransitions = mconcat
+        [ nfaTransitions nfa_a
+        , nfaTransitions nfa_b
+        , transitionsFromList
+          [ (Nothing, final_a, start_b)
+          | final_a <- Set.toList (nfaFinal nfa_a)
+          , start_b <- Set.toList (nfaStart nfa_b)
+          ]
+        ]
+      }
+  Alt a b -> do
+    nfa_a <- compileM a
+    nfa_b <- compileM b
+    return $ NFA
+      { nfaStart = nfaStart nfa_a <> nfaStart nfa_b
+      , nfaFinal = nfaFinal nfa_a <> nfaFinal nfa_b
+      , nfaTransitions = nfaTransitions nfa_a <> nfaTransitions nfa_b
+      }
+  Plus a -> do
+    nfa_a <- compileM a
+    return $ NFA
+      { nfaStart = nfaStart nfa_a
+      , nfaFinal = nfaFinal nfa_a
+      , nfaTransitions =
+        nfaTransitions nfa_a <>
+        transitionsFromList
+        [ (Nothing, f, s)
+        | s <- Set.toList $ nfaStart nfa_a
+        , f <- Set.toList $ nfaFinal nfa_a
+        ]
+      }
+  Star a -> compileM $ Alt (Plus a) Empty
+
+  where
+    fresh :: State Int NfaState
+    fresh = do
+      n <- get
+      put $! n+1
+      return $ NfaState n
 
 follow :: Ord c => [[c]] -> NfaTransitions (Maybe c)
 follow = transitionsFromList . concat . zipWith follow1 [0..]
